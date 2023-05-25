d4755dba3b93c81a2e03099a1f32c891dd2f4982
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-05-25 15:38:12 +0300

    Update counts_mod.pl

--- counts_mod.pl
+++ counts_mod.pl
@@ -5,32 +5,21 @@ use strict;
 open ( IN, $ARGV[0] );
 
 while ( my $line = <IN> ) {
-
     chomp $line;
-
     $line =~ s/\.s\.bam|gene:|results\///g;
 
     if ( $line =~ /\^|#/ ) {
-
 	next;
-
     }
     
-    
-    my @f = split (/\t/,$line);
-    
+    my @f = split (/\t/,$line);   
     print "$f[0]\t$f[6]\t";
     
     for my $i ( 7..scalar(@f) - 1 ) {
-
 	print "\t";
-
 	print "$f[$i]";
-
     }
-
     print "\n";
-
 }
 
 
